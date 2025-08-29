c8533eef3a769096cbcdd839c3db2a1f1f278c5f
OJPARKINSON <olliepar01@gmail.com>
2025-08-29 10:47:52 +0100

    added short code gen

--- go/handlers/shortenHandler.go
+++ go/handlers/shortenHandler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -27,37 +28,55 @@ type ShortenHandler struct{}
 
 func (s *ShortenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-
-		collection, dberr := db.ConnectToCollection()
-		if dberr != nil {
-			fmt.Println("failed to connect to the db: ", dberr)
-		}
-
-		var reqBody shortenReq
-		err := json.NewDecoder(r.Body).Decode(&reqBody)
-		if err != nil {
-			fmt.Println("Error decoding body: ", err)
-			w.WriteHeader(400)
-		}
-
-		url := url{Url: reqBody.URL, ShortCode: "abc123", CreatedAt: time.Now(), UpdatedAt: time.Now()}
-
-		result, insertErr := collection.InsertOne(context.TODO(), url)
-		if insertErr != nil {
-			fmt.Println("Error inserting url: ", err)
-			w.WriteHeader(500)
-		} else if result.Acknowledged == false {
-			fmt.Println("insert not acknowledged: ", err)
-			w.WriteHeader(500)
-		}
-
-		url.Id = result.InsertedID.(bson.ObjectID).Hex()
-
-		fmt.Println(result.Acknowledged, result.InsertedID)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(201)
-		json.NewEncoder(w).Encode(url)
+		CreateShorten(w, r)
+		return
 	}
 
 	w.WriteHeader(404)
+}
+
+func CreateShorten(w http.ResponseWriter, r *http.Request) {
+	collection, dberr := db.ConnectToCollection()
+	if dberr != nil {
+		fmt.Println("failed to connect to the db: ", dberr)
+	}
+
+	var reqBody shortenReq
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println("Error decoding body: ", err)
+		w.WriteHeader(400)
+	}
+
+	shortCode := generateShortCode()
+	url := url{Url: reqBody.URL, ShortCode: shortCode, CreatedAt: time.Now(), UpdatedAt: time.Now()}
+
+	result, insertErr := collection.InsertOne(context.TODO(), url)
+	if insertErr != nil {
+		fmt.Println("Error inserting url: ", err)
+		w.WriteHeader(500)
+	} else if result.Acknowledged == false {
+		fmt.Println("insert not acknowledged: ", err)
+		w.WriteHeader(500)
+	}
+
+	url.Id = result.InsertedID.(bson.ObjectID).Hex()
+
+	fmt.Println(result.Acknowledged, result.InsertedID)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(url)
+}
+
+func generateShortCode() string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	result := make([]byte, 6)
+	for i := range result {
+		result[i] = charset[random.Intn(len(charset))]
+	}
+
+	return string(result)
 }
