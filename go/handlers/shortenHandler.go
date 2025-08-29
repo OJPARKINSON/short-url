577ef70baed286d1f2ca36e921f865b515139909
OJPARKINSON <olliepar01@gmail.com>
2025-08-29 12:45:23 +0100

    added update endpoint

--- go/handlers/shortenHandler.go
+++ go/handlers/shortenHandler.go
@@ -34,6 +34,9 @@ func (s *ShortenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		s.CreateShorten(w, r)
 		return
+	case http.MethodPut:
+		s.UpdateShorten(w, r)
+		return
 	}
 
 	w.WriteHeader(404)
@@ -98,6 +101,52 @@ func (s *ShortenHandler) GetShorten(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(reqBody)
+}
+
+func (s *ShortenHandler) UpdateShorten(w http.ResponseWriter, r *http.Request) {
+	shortCode := r.PathValue("shortcode")
+	if shortCode == "" {
+		http.Error(w, "Short code required", http.StatusBadRequest)
+		return
+	}
+
+	var reqBody shortenReq
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println("Error decoding body: ", err)
+		http.Error(w, "Error decoding body", http.StatusBadRequest)
+		return
+	}
+
+	collection, dberr := db.ConnectToCollection()
+	if dberr != nil {
+		http.Error(w, "Failed to connect to the DB", http.StatusInternalServerError)
+		return
+	}
+
+	filter := bson.D{{Key: "shortcode", Value: shortCode}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "url", Value: reqBody.URL}, {Key: "updatedat", Value: time.Now()}}}}
+	result, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil || !result.Acknowledged {
+		http.Error(w, "Failed to upate url", http.StatusInternalServerError)
+	}
+
+	getFilter := bson.D{{Key: "shortcode", Value: shortCode}}
+
+	var getReqBody url
+	getResult := collection.FindOne(context.TODO(), getFilter).Decode(&getReqBody)
+	if getResult != nil {
+		if getResult == mongo.ErrNoDocuments {
+			http.Error(w, "Failed to find the short url", http.StatusNotFound)
+		} else {
+			http.Error(w, "Failed to get the document", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(getReqBody)
+
 }
 
 func generateShortCode() string {
