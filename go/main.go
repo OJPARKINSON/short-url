35d988d858199a5220cd847b854c42be77af2399
Oliver Parkinson <olliepar01@gmail.com>
2025-09-13 09:00:50 +0000

    udpated env error handling

--- go/main.go
+++ go/main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file, please follow the .env example")
 	}
 	db.Init()
 
